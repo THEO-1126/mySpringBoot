c622635538e30069a66b8fd09817a9321409c135
THEO-1126 <1223230728@qq.com>
2023-01-16 00:18:44 +0800

    SpringBoot项目，编写配置文件，各层结构

---
+++ src/main/java/cn/edu/guet/dao/BookDao.java
@@ -0,0 +1,14 @@
+package cn.edu.guet.dao;
+import cn.edu.guet.bean.Book;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @Author liwei
+ * @Date 2023/1/9 17:45
+ * @Version 1.0
+ */
+
+@Mapper
+public interface BookDao {
+    void saveBook(Book book);
+}
